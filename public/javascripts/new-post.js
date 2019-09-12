3445d3f16a3910ff0f80d3bb7d61e2bf80722274
Dusty <dustygaron@hotmail.com>
2019-09-12 09:13:51 -0400

    merged

--- public/javascripts/new-post.js
+++ public/javascripts/new-post.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
 
 
-  document.getElementById('create-post').onsubmit = function(event){
+  document.getElementById('create-post').onsubmit = function (event) {
 
     event.preventDefault();
 
@@ -13,56 +13,56 @@ document.addEventListener('DOMContentLoaded', () => {
     let image = document.getElementById('upload-file').files[0];
 
 
-const formData = new FormData();
+    const formData = new FormData();
 
-formData.append('postTitle', title);
-formData.append('postDate', date);
-formData.append('postDescription', description);
-formData.append('postChildId', childId);
-formData.append('postImage', image);
+    formData.append('postTitle', title);
+    formData.append('postDate', date);
+    formData.append('postDescription', description);
+    formData.append('postChildId', childId);
+    formData.append('postImage', image);
 
 
-// make axios request and send the correct stuff in req.body
-     axios.post('/api/feed/new-post', formData)
-     .then((result)=>{
+    // make axios request and send the correct stuff in req.body
+    axios.post('/api/feed/new-post', formData)
+      .then((result) => {
 
         console.log(result.data.newPost._id)
-    
-      
-        axios.get('/api/feed/created-post/'+result.data.newPost._id)
-          .then((response)=>{
-           // take the new updated info and put it on the page
-           
-           console.log(response)
-           
-           let createdPost = response.data
+
+
+        axios.get('/api/feed/created-post/' + result.data.newPost._id)
+          .then((response) => {
+            // take the new updated info and put it on the page
+
+            console.log(response)
+
+            let createdPost = response.data
 
             console.log(createdPost)
 
-           let theTitle = createdPost.title
-           let theDate = createdPost.creation
-           let theDescription = createdPost.description
-           let theImage = createdPost.image
-   
-           console.log(theTitle, theDate, theDescription, theImage)
-   
+            let theTitle = createdPost.title
+            let theDate = createdPost.creation
+            let theDescription = createdPost.description
+            let theImage = createdPost.image
 
-          $(".post-images").prepend(`<div class="col s12 m3 push-s5">
+            console.log(theTitle, theDate, theDescription, theImage)
+
+
+            $(".post-images").prepend(`<div class="col s12 m3 push-s5">
           <img src="${theImage}" alt="" class="responsive-img materialboxed  art-img append-test">
           </div>`)
 
-      
-       })
-       .catch((err)=>{
-           console.log(err);
-        })
+
+          })
+          .catch((err) => {
+            console.log(err);
+          })
       })
-       .catch((err)=>{
+      .catch((err) => {
         console.log(err);
       })
 
-    
-    
-    }  // end of on-click function
 
-  }, false); // end of document event listener
+
+  }  // end of on-click function
+
+}, false); // end of document event listener
